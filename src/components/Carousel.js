b20bbf1da6a0e93c7ca1c704ff7c423584982583
Jake Bogan <jakebogan01@gmail.com>
2022-10-05 18:52:47 -0400

    switch to using a loop for trending cards

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -7,6 +7,30 @@ import Bookmark from "./Bookmark";
 
 export default class Carousel extends Component {
     render() {
+        const cards = [];
+        for (let i = 1; i <= 5; i++) {
+            cards.push(
+                <div>
+                    <div className="relative flex flex-col bg-cover bg-center bg-no-repeat" style={{backgroundImage: "url('https://www.outsideonline.com/wp-content/uploads/2020/02/03/traveling-with-anxiety_h.jpg')"}}>
+                        <Bookmark />
+                        <div className="group p-4">
+                            <div className="md:group-hover:opacity-100 opacity-0 flex justify-center items-center absolute inset-0 w-full h-full bg-black bg-opacity-50 z-10 transition-opacity duration-300">
+                                <button type="button" className="flex justify-center items-center bg-white bg-opacity-25 text-white rounded-full w-[7.3125rem] h-12 cursor-pointer">
+                                        <span className="flex items-center space-x-5">
+                                            <svg width="30" height="30" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M0 15C0 6.713 6.713 0 15 0c8.288 0 15 6.713 15 15 0 8.288-6.712 15-15 15-8.287 0-15-6.712-15-15Zm21-.5L12 8v13l9-6.5Z" /></svg>
+                                            <span>Play</span>
+                                        </span>
+                                </button>
+                            </div>
+                            <div className="md:group-hover:opacity-0 flex flex-col justify-end transition-opacity duration-300">
+                                <p className="font-light text-white text-xs">2019 &#160;&#183;&#160; <svg width="12" height="12" fill="none" className="inline transform -translate-y-[0.06rem]" xmlns="http://www.w3.org/2000/svg"><path opacity=".75" fillRule="evenodd" clipRule="evenodd" d="M10.173 0H1.827A1.827 1.827 0 0 0 0 1.827v8.346C0 11.183.818 12 1.827 12h8.346A1.827 1.827 0 0 0 12 10.173V1.827A1.827 1.827 0 0 0 10.173 0ZM2.4 5.4H1.2V4.2h1.2v1.2Zm0 1.2H1.2v1.2h1.2V6.6Zm8.4-1.2H9.6V4.2h1.2v1.2Zm0 1.2H9.6v1.2h1.2V6.6Zm0-4.956V2.4H9.6V1.2h.756a.444.444 0 0 1 .444.444ZM2.4 1.2h-.756a.444.444 0 0 0-.444.444V2.4h1.2V1.2Zm-1.2 9.156V9.6h1.2v1.2h-.756a.444.444 0 0 1-.444-.444Zm9.156.444a.444.444 0 0 0 .444-.444V9.6H9.6v1.2h.756Z" fill="#fff"/></svg>&#160; Movie &#160;&#183;&#160; PG</p>
+                                <h3 className="text-white font-medium text-[0.9375rem]">Beyond Earth</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
         const settings = {
             className: "slider variable-width",
             dots: false,
@@ -22,101 +46,7 @@ export default class Carousel extends Component {
             <section>
                 <h2 className="font-light text-xl text-white pl-4 mb-4">Trending</h2>
                 <Slider {...settings}>
-                    <div>
-                        <div className="relative flex flex-col bg-cover bg-center bg-no-repeat" style={{backgroundImage: "url('https://www.outsideonline.com/wp-content/uploads/2020/02/03/traveling-with-anxiety_h.jpg')"}}>
-                            <Bookmark />
-                            <div className="group p-4">
-                                <div className="md:group-hover:opacity-100 opacity-0 flex justify-center items-center absolute inset-0 w-full h-full bg-black bg-opacity-50 z-10 transition-opacity duration-300">
-                                    <button type="button" className="flex justify-center items-center bg-white bg-opacity-25 text-white rounded-full w-[7.3125rem] h-12 cursor-pointer">
-                                        <span className="flex items-center space-x-5">
-                                            <svg width="30" height="30" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M0 15C0 6.713 6.713 0 15 0c8.288 0 15 6.713 15 15 0 8.288-6.712 15-15 15-8.287 0-15-6.712-15-15Zm21-.5L12 8v13l9-6.5Z" /></svg>
-                                            <span>Play</span>
-                                        </span>
-                                    </button>
-                                </div>
-                                <div className="md:group-hover:opacity-0 flex flex-col justify-end transition-opacity duration-300">
-                                    <p className="font-light text-white text-xs">2019 &#160;&#183;&#160; <svg width="12" height="12" fill="none" className="inline transform -translate-y-[0.06rem]" xmlns="http://www.w3.org/2000/svg"><path opacity=".75" fillRule="evenodd" clipRule="evenodd" d="M10.173 0H1.827A1.827 1.827 0 0 0 0 1.827v8.346C0 11.183.818 12 1.827 12h8.346A1.827 1.827 0 0 0 12 10.173V1.827A1.827 1.827 0 0 0 10.173 0ZM2.4 5.4H1.2V4.2h1.2v1.2Zm0 1.2H1.2v1.2h1.2V6.6Zm8.4-1.2H9.6V4.2h1.2v1.2Zm0 1.2H9.6v1.2h1.2V6.6Zm0-4.956V2.4H9.6V1.2h.756a.444.444 0 0 1 .444.444ZM2.4 1.2h-.756a.444.444 0 0 0-.444.444V2.4h1.2V1.2Zm-1.2 9.156V9.6h1.2v1.2h-.756a.444.444 0 0 1-.444-.444Zm9.156.444a.444.444 0 0 0 .444-.444V9.6H9.6v1.2h.756Z" fill="#fff"/></svg>&#160; Movie &#160;&#183;&#160; PG</p>
-                                    <h3 className="text-white font-medium text-[0.9375rem]">Beyond Earth</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div className="relative flex flex-col bg-cover bg-center bg-no-repeat" style={{backgroundImage: "url('https://www.outsideonline.com/wp-content/uploads/2020/02/03/traveling-with-anxiety_h.jpg')"}}>
-                            <Bookmark />
-                            <div className="group p-4">
-                                <div className="md:group-hover:opacity-100 opacity-0 flex justify-center items-center absolute inset-0 w-full h-full bg-black bg-opacity-50 z-10 transition-opacity duration-300">
-                                    <button type="button" className="flex justify-center items-center bg-white bg-opacity-25 text-white rounded-full w-[7.3125rem] h-12 cursor-pointer">
-                                        <span className="flex items-center space-x-5">
-                                            <svg width="30" height="30" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M0 15C0 6.713 6.713 0 15 0c8.288 0 15 6.713 15 15 0 8.288-6.712 15-15 15-8.287 0-15-6.712-15-15Zm21-.5L12 8v13l9-6.5Z" /></svg>
-                                            <span>Play</span>
-                                        </span>
-                                    </button>
-                                </div>
-                                <div className="md:group-hover:opacity-0 flex flex-col justify-end transition-opacity duration-300">
-                                    <p className="font-light text-white text-xs">2019 &#160;&#183;&#160; <svg width="12" height="12" fill="none" className="inline transform -translate-y-[0.06rem]" xmlns="http://www.w3.org/2000/svg"><path opacity=".75" fillRule="evenodd" clipRule="evenodd" d="M10.173 0H1.827A1.827 1.827 0 0 0 0 1.827v8.346C0 11.183.818 12 1.827 12h8.346A1.827 1.827 0 0 0 12 10.173V1.827A1.827 1.827 0 0 0 10.173 0ZM2.4 5.4H1.2V4.2h1.2v1.2Zm0 1.2H1.2v1.2h1.2V6.6Zm8.4-1.2H9.6V4.2h1.2v1.2Zm0 1.2H9.6v1.2h1.2V6.6Zm0-4.956V2.4H9.6V1.2h.756a.444.444 0 0 1 .444.444ZM2.4 1.2h-.756a.444.444 0 0 0-.444.444V2.4h1.2V1.2Zm-1.2 9.156V9.6h1.2v1.2h-.756a.444.444 0 0 1-.444-.444Zm9.156.444a.444.444 0 0 0 .444-.444V9.6H9.6v1.2h.756Z" fill="#fff"/></svg>&#160; Movie &#160;&#183;&#160; PG</p>
-                                    <h3 className="text-white font-medium text-[0.9375rem]">Beyond Earth</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div className="relative flex flex-col bg-cover bg-center bg-no-repeat" style={{backgroundImage: "url('https://www.outsideonline.com/wp-content/uploads/2020/02/03/traveling-with-anxiety_h.jpg')"}}>
-                            <Bookmark />
-                            <div className="group p-4">
-                                <div className="md:group-hover:opacity-100 opacity-0 flex justify-center items-center absolute inset-0 w-full h-full bg-black bg-opacity-50 z-10 transition-opacity duration-300">
-                                    <button type="button" className="flex justify-center items-center bg-white bg-opacity-25 text-white rounded-full w-[7.3125rem] h-12 cursor-pointer">
-                                        <span className="flex items-center space-x-5">
-                                            <svg width="30" height="30" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M0 15C0 6.713 6.713 0 15 0c8.288 0 15 6.713 15 15 0 8.288-6.712 15-15 15-8.287 0-15-6.712-15-15Zm21-.5L12 8v13l9-6.5Z" /></svg>
-                                            <span>Play</span>
-                                        </span>
-                                    </button>
-                                </div>
-                                <div className="md:group-hover:opacity-0 flex flex-col justify-end transition-opacity duration-300">
-                                    <p className="font-light text-white text-xs">2019 &#160;&#183;&#160; <svg width="12" height="12" fill="none" className="inline transform -translate-y-[0.06rem]" xmlns="http://www.w3.org/2000/svg"><path opacity=".75" fillRule="evenodd" clipRule="evenodd" d="M10.173 0H1.827A1.827 1.827 0 0 0 0 1.827v8.346C0 11.183.818 12 1.827 12h8.346A1.827 1.827 0 0 0 12 10.173V1.827A1.827 1.827 0 0 0 10.173 0ZM2.4 5.4H1.2V4.2h1.2v1.2Zm0 1.2H1.2v1.2h1.2V6.6Zm8.4-1.2H9.6V4.2h1.2v1.2Zm0 1.2H9.6v1.2h1.2V6.6Zm0-4.956V2.4H9.6V1.2h.756a.444.444 0 0 1 .444.444ZM2.4 1.2h-.756a.444.444 0 0 0-.444.444V2.4h1.2V1.2Zm-1.2 9.156V9.6h1.2v1.2h-.756a.444.444 0 0 1-.444-.444Zm9.156.444a.444.444 0 0 0 .444-.444V9.6H9.6v1.2h.756Z" fill="#fff"/></svg>&#160; Movie &#160;&#183;&#160; PG</p>
-                                    <h3 className="text-white font-medium text-[0.9375rem]">Beyond Earth</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div className="relative flex flex-col bg-cover bg-center bg-no-repeat" style={{backgroundImage: "url('https://www.outsideonline.com/wp-content/uploads/2020/02/03/traveling-with-anxiety_h.jpg')"}}>
-                            <Bookmark />
-                            <div className="group p-4">
-                                <div className="md:group-hover:opacity-100 opacity-0 flex justify-center items-center absolute inset-0 w-full h-full bg-black bg-opacity-50 z-10 transition-opacity duration-300">
-                                    <button type="button" className="flex justify-center items-center bg-white bg-opacity-25 text-white rounded-full w-[7.3125rem] h-12 cursor-pointer">
-                                        <span className="flex items-center space-x-5">
-                                            <svg width="30" height="30" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M0 15C0 6.713 6.713 0 15 0c8.288 0 15 6.713 15 15 0 8.288-6.712 15-15 15-8.287 0-15-6.712-15-15Zm21-.5L12 8v13l9-6.5Z" /></svg>
-                                            <span>Play</span>
-                                        </span>
-                                    </button>
-                                </div>
-                                <div className="md:group-hover:opacity-0 flex flex-col justify-end transition-opacity duration-300">
-                                    <p className="font-light text-white text-xs">2019 &#160;&#183;&#160; <svg width="12" height="12" fill="none" className="inline transform -translate-y-[0.06rem]" xmlns="http://www.w3.org/2000/svg"><path opacity=".75" fillRule="evenodd" clipRule="evenodd" d="M10.173 0H1.827A1.827 1.827 0 0 0 0 1.827v8.346C0 11.183.818 12 1.827 12h8.346A1.827 1.827 0 0 0 12 10.173V1.827A1.827 1.827 0 0 0 10.173 0ZM2.4 5.4H1.2V4.2h1.2v1.2Zm0 1.2H1.2v1.2h1.2V6.6Zm8.4-1.2H9.6V4.2h1.2v1.2Zm0 1.2H9.6v1.2h1.2V6.6Zm0-4.956V2.4H9.6V1.2h.756a.444.444 0 0 1 .444.444ZM2.4 1.2h-.756a.444.444 0 0 0-.444.444V2.4h1.2V1.2Zm-1.2 9.156V9.6h1.2v1.2h-.756a.444.444 0 0 1-.444-.444Zm9.156.444a.444.444 0 0 0 .444-.444V9.6H9.6v1.2h.756Z" fill="#fff"/></svg>&#160; Movie &#160;&#183;&#160; PG</p>
-                                    <h3 className="text-white font-medium text-[0.9375rem]">Beyond Earth</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div className="relative flex flex-col bg-cover bg-center bg-no-repeat" style={{backgroundImage: "url('https://www.outsideonline.com/wp-content/uploads/2020/02/03/traveling-with-anxiety_h.jpg')"}}>
-                            <Bookmark />
-                            <div className="group p-4">
-                                <div className="md:group-hover:opacity-100 opacity-0 flex justify-center items-center absolute inset-0 w-full h-full bg-black bg-opacity-50 z-10 transition-opacity duration-300">
-                                    <button type="button" className="flex justify-center items-center bg-white bg-opacity-25 text-white rounded-full w-[7.3125rem] h-12 cursor-pointer">
-                                        <span className="flex items-center space-x-5">
-                                            <svg width="30" height="30" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M0 15C0 6.713 6.713 0 15 0c8.288 0 15 6.713 15 15 0 8.288-6.712 15-15 15-8.287 0-15-6.712-15-15Zm21-.5L12 8v13l9-6.5Z" /></svg>
-                                            <span>Play</span>
-                                        </span>
-                                    </button>
-                                </div>
-                                <div className="md:group-hover:opacity-0 flex flex-col justify-end transition-opacity duration-300">
-                                    <p className="font-light text-white text-xs">2019 &#160;&#183;&#160; <svg width="12" height="12" fill="none" className="inline transform -translate-y-[0.06rem]" xmlns="http://www.w3.org/2000/svg"><path opacity=".75" fillRule="evenodd" clipRule="evenodd" d="M10.173 0H1.827A1.827 1.827 0 0 0 0 1.827v8.346C0 11.183.818 12 1.827 12h8.346A1.827 1.827 0 0 0 12 10.173V1.827A1.827 1.827 0 0 0 10.173 0ZM2.4 5.4H1.2V4.2h1.2v1.2Zm0 1.2H1.2v1.2h1.2V6.6Zm8.4-1.2H9.6V4.2h1.2v1.2Zm0 1.2H9.6v1.2h1.2V6.6Zm0-4.956V2.4H9.6V1.2h.756a.444.444 0 0 1 .444.444ZM2.4 1.2h-.756a.444.444 0 0 0-.444.444V2.4h1.2V1.2Zm-1.2 9.156V9.6h1.2v1.2h-.756a.444.444 0 0 1-.444-.444Zm9.156.444a.444.444 0 0 0 .444-.444V9.6H9.6v1.2h.756Z" fill="#fff"/></svg>&#160; Movie &#160;&#183;&#160; PG</p>
-                                    <h3 className="text-white font-medium text-[0.9375rem]">Beyond Earth</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    { cards }
                 </Slider>
             </section>
         );
